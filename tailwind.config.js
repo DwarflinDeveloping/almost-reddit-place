d4fabcf23a78754e32e55bcbfeb9545ccaad9b40
DwarflinDeveloping <dwarflin@gmx.de>
2022-04-30 10:57:50 +0200

    new features

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+      extend: {
+        colors: {
+          "auth-gray": {
+            50: "#f6f9fe",
+            100: "#eef4fc",
+            200: "#dce5f6",
+            300: "#c2d0ea",
+            400: "#869bc6",
+            500: "#556b9a",
+            600: "#3a4d76",
+            700: "#263962",
+            800: "#132146",
+            900: "#040d30",
+          },
+        },
+      },
+    },
+  plugins: [],
+  darkMode: 'class',
+}
